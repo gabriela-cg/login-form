5a77740f26432b0979c52979ff049379179d799f
Gabriela Gonçalves <111512062+gabriela-cg@users.noreply.github.com>
2023-10-16 21:30:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import logo from './assets/sustentacao2.png'
+import './'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">
+              <img src={logo} alt=""/>
+            </span>
+
+            {/* input de email */}
+            <div className="wrap-input">
+              <input type="email"/>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            {/* input de senha */}
+            <div className="wrap-input">
+              <input type="password"/>
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            {/* botão */}
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Entrar</button>              
+            </div>
+
+            {/* esqueci minha senha */}
+            <div className="text-center">
+              <span className="txt1" hr>Esqueceu sua senha?</span>
+              <a className="txt2" href="#">Clique aqui</a>   
+                           
+            </div>
+
+
+
+          </form>
+
+        </div>
+      </div>
     </div>
+    
+    
   );
 }
 
